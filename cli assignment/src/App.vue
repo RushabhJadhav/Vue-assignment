d1226566effb5af2b8314c3844de80c3d85eb3b5
RushabhJadhav <86244092+RushabhJadhav@users.noreply.github.com>
2023-06-14 11:14:00 +0530

    Add files via upload

---
+++ cli assignment/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+    <app-main v-bind:ninjas="ninjas"></app-main>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Main from './components/Main.vue';
+
+export default {
+  data() {
+    return {
+      ninjas: [
+        {name: 'Ryu', speciality: 'Vue Components', show: false},
+        {name: 'Crystal', speciality: 'HTML Wizardy', show: false},
+        {name: 'Hitoshi', speciality: 'Click Events', show: false},
+        {name: 'Tango', speciality: 'Conditionals', show: false},
+        {name: 'Kami', speciality: 'Webpack', show: false},
+        {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+      ]
+    }
+  },
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-main': Main
+  }
+}
+
+</script>
+
+<style scoped></style>
